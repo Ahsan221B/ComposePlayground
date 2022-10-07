09421198bdb7f3c670b95abeb6541195f587ecda
Ahsan Mustafa <ahsanmustafa2015@gmail.com>
2022-10-07 14:23:28 +0500

    L1: Composable Functions
    + Defined composable functions
    + Added different elements in my composable
    + Structured the UI component using layout composables
    + Extended composables by using modifiers
    + Created an efficient list
    + Kept track of state and modified it
    + Added user interaction on a composable
    + Animated messages while expanding them

---
+++ app/src/main/java/com/iahsanmustafa/composeplayground/MainActivity.kt
@@ -0,0 +1,124 @@
+package com.iahsanmustafa.composeplayground
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.iahsanmustafa.composeplayground.data.Message
+import com.iahsanmustafa.composeplayground.data.SampleData
+import com.iahsanmustafa.composeplayground.ui.theme.ComposePlaygroundTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposePlaygroundTheme {
+                Conversation(SampleData.conversationSample)
+            }
+        }
+    }
+}
+
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+    ComposePlaygroundTheme {
+        Surface {
+            MessageCard(
+                message = Message(
+                    author = "Ahsan",
+                    body = "Hey, how are you?"
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun MessageCard(message: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact Profile Picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // Keeping track if the message is expanded or not in this variable
+        var isExpanded by remember { mutableStateOf(false) }
+
+        val surfaceColor by animateColorAsState(
+            targetValue = if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = message.author,
+                color = MaterialTheme.colors.secondary,
+                style = MaterialTheme.typography.subtitle2
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = message.body,
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, display all its content
+                    // otherwise only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    ComposePlaygroundTheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
---
+++ app/src/main/java/com/iahsanmustafa/composeplayground/data/Message.kt
@@ -0,0 +1,7 @@
+package com.iahsanmustafa.composeplayground.data
+
+
+data class Message(
+    val author: String,
+    val body: String
+)
